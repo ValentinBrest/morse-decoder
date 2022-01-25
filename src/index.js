306204261f1039c42d3318c647af84ee10e92f3b
Valiantsin Kanapatski <79157063+ValentinBrest@users.noreply.github.com>
2022-01-25 14:58:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let total = ''; 
+    for (let i = 0; i < expr.length; i = i + 10) {
+        let morseSimbol = expr.slice(i, i + 10);
+        if (morseSimbol == '**********') {
+            total = total + ' '
+        } else  {
+            let arr = []
+            for (let n = 0; n < morseSimbol.length; n = n + 2) {
+                let num = morseSimbol.slice(n, n + 2)
+                if (num == '11') {
+                    arr.push('-')
+                } else if (num == '10'){
+                    arr.push('.')
+                }
+            }
+            let str = arr.join('');
+            arr = [];
+            let letter
+                for (key in MORSE_TABLE) {
+                    if (key == str) {
+                        letter = MORSE_TABLE[key]
+                        total = total + letter
+                    }
+                }
+        }
+    }
+    return total
 }
 
 module.exports = {
